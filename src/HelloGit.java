1a12fb00b7f03aaab2f4903a3878655a3106a2e5
LeeKinyou <1412901165@qq.com>
2022-10-15 13:58:20 +0800

    第二次提交

---
+++ src/HelloGit.java
@@ -0,0 +1,6 @@
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("Now We will start our coding lives!");
+    }
+}
